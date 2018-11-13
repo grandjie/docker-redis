f0004d89ca147099ed4e96d645f6c6be5ce90a88
ny <niyaogoo@163.com>
2018-11-13 20:45:59 +0800

    remove sth

--- 5.0/redis-cli.sh
+++ 5.0/redis-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                                                                                                                                                        
 host_ip=$(ip route | awk '/default/ { print $9 }')
-sudo docker exec -it redis redis-cli -a xs1h -h ${host_ip} -c
+sudo docker exec -it redis redis-cli -h ${host_ip} -c
